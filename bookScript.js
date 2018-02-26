4a99d90df7ffe95821a4b42fbdb5146e64443dac
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2018-02-26 10:55:08 +0530

    Adding basic script to download the file from extension

--- bookScript.js
+++ bookScript.js
@@ -1,5 +1,29 @@
 console.log('loading the bookScript');
-chrome.bookmarks.getTree(function (results){
+chrome.bookmarks.getTree(function (results) {
 	console.log('getTree=>', results);
 
 });
+
+var backupButton = document.querySelector('#link');
+backupButton.addEventListener('click', function () {
+	console.log('backupButton clicked=>');
+	chrome.bookmarks.getTree(function (results) {
+		console.log('getTree=>', results);
+		// download('test.txt', 'Hello world!');
+		download('test.html', '<h1>hello</h1>');
+	});
+});
+
+function download(filename, text) {
+	var pom = document.createElement('a');
+	// pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+	pom.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(text));
+	pom.setAttribute('download', filename);
+	if (document.createEvent) {
+		var event = document.createEvent('MouseEvents');
+		event.initEvent('click', true, true);
+		pom.dispatchEvent(event);
+	} else {
+		pom.click();
+	}
+}
