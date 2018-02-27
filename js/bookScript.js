803ac1eca1016a6a44bbb7e60698d15e2664f168
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2018-02-27 15:08:56 +0530

    Adding traversing code from all nodes

--- js/bookScript.js
+++ js/bookScript.js
@@ -1,30 +1,45 @@
 console.log('loading the bookScript');
+var bmMarkup = '';
+
 chrome.bookmarks.getTree(function (results) {
 	console.log('getTree=>', results);
-
 });
 
 var backupButton = document.querySelector('#link');
 backupButton.addEventListener('click', function () {
 	console.log('backupButton clicked=>');
+	bmMarkup = '';
 	chrome.bookmarks.getTree(function (results) {
 		console.log('getTree=>', results);
-		// download('test.txt', 'Hello world!');
-		// download('test.html', '<h1>hello</h1>');
-		printBM(results);
+		traverseBookmarks(results);
+		// console.log('bmMarkup=>', bmMarkup);
+		// download('test.html', bmMarkup);
 	});
+});
 
-	function printBM(bmObject) {
-		for (var key in bmObject) {
-			if (typeof(bmObject[key]) == 'object') {
-				printBM(bmObject[key]);
-			} else {
-				console.log(key + ": " + bmObject[key]);
-			}
+function traverseBookmarks(bookmarkTreeNodes) {
+	for (var i = 0; i < bookmarkTreeNodes.length; i++) {
+		var title, url, isFolder = false;
+		if (bookmarkTreeNodes[i].url) {
+			url = bookmarkTreeNodes[i].url;
+			isFolder = false;
+		} else {
+			isFolder = true;
+		}
+		title = bookmarkTreeNodes[i].title;
+		if (isFolder) {
+			bmMarkup += '<h1>' + title + '</h1>';
+			console.log('%cfolder>' + title, 'color:green');
+		} else {
+			bmMarkup += '<li><a href="' + url + '" target="_blank">' + title + '</a></li>';
+			console.log('		title>' + title + '->(' + url + ')');
+			// console.log('		url  >', url);
+		}
+		if (bookmarkTreeNodes[i].children) {
+			traverseBookmarks(bookmarkTreeNodes[i].children);
 		}
 	}
-
-});
+}
 
 function download(filename, text) {
 	var pom = document.createElement('a');
