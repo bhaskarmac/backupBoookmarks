daeee559ab5a35b94548e36232e69ade93c8635e
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2018-02-27 15:20:48 +0530

    Improved UI and added comments in code

--- js/bookScript.js
+++ js/bookScript.js
@@ -1,22 +1,23 @@
 console.log('loading the bookScript');
 var bmMarkup = '';
+var backupButton = document.querySelector('#btnBackup');
 
-chrome.bookmarks.getTree(function (results) {
-	console.log('getTree=>', results);
-});
-
-var backupButton = document.querySelector('#link');
+//Adding event listener for the backup button
 backupButton.addEventListener('click', function () {
 	console.log('backupButton clicked=>');
 	bmMarkup = '';
+	//API call to get the all bookmarks
 	chrome.bookmarks.getTree(function (results) {
-		console.log('getTree=>', results);
 		traverseBookmarks(results);
 		// console.log('bmMarkup=>', bmMarkup);
 		download('test.html', bmMarkup);
 	});
 });
 
+/**
+ * Method to traverse through bookmarks Object array returned from API
+ * @param {JSON Object Array} bookmarkTreeNodes 
+ */
 function traverseBookmarks(bookmarkTreeNodes) {
 	for (var i = 0; i < bookmarkTreeNodes.length; i++) {
 		var title, url, isFolder = false;
@@ -29,11 +30,10 @@ function traverseBookmarks(bookmarkTreeNodes) {
 		title = bookmarkTreeNodes[i].title;
 		if (isFolder) {
 			bmMarkup += '<h1>' + title + '</h1>';
-			console.log('%cfolder>' + title, 'color:green');
+			// console.log('%cfolder>' + title, 'color:green');
 		} else {
 			bmMarkup += '<li><a href="' + url + '" target="_blank">' + title + '</a></li>';
-			console.log('		title>' + title + '->(' + url + ')');
-			// console.log('		url  >', url);
+			// console.log('		title>' + title + '->(' + url + ')');
 		}
 		if (bookmarkTreeNodes[i].children) {
 			traverseBookmarks(bookmarkTreeNodes[i].children);
@@ -41,16 +41,20 @@ function traverseBookmarks(bookmarkTreeNodes) {
 	}
 }
 
+/**
+ * Method to download the file with bookmarks
+ * @param {string} filename 
+ * @param {string} text 
+ */
 function download(filename, text) {
-	var pom = document.createElement('a');
-	// pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-	pom.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(text));
-	pom.setAttribute('download', filename);
+	var downloadLinkNode = document.createElement('a');
+	downloadLinkNode.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(text));
+	downloadLinkNode.setAttribute('download', filename);
 	if (document.createEvent) {
 		var event = document.createEvent('MouseEvents');
 		event.initEvent('click', true, true);
-		pom.dispatchEvent(event);
+		downloadLinkNode.dispatchEvent(event);
 	} else {
-		pom.click();
+		downloadLinkNode.click();
 	}
 }
